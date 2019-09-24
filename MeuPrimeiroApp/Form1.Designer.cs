40c4c9a639ebd7c4e7b1659a802cf617e989f0e2
Alan Dalcastagne da Cunha <ate45@UNI4.local>
2019-09-24 20:37:34 -0300

    Add project files.

---
+++ MeuPrimeiroApp/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace MeuPrimeiroApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Desligar = new System.Windows.Forms.Button();
+            this.Detalhes = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(294, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ligar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Ligar_Click);
+            // 
+            // Desligar
+            // 
+            this.Desligar.Location = new System.Drawing.Point(402, 82);
+            this.Desligar.Name = "Desligar";
+            this.Desligar.Size = new System.Drawing.Size(99, 55);
+            this.Desligar.TabIndex = 1;
+            this.Desligar.Text = "Desligar";
+            this.Desligar.UseVisualStyleBackColor = true;
+            this.Desligar.Click += new System.EventHandler(this.Desligar_Click);
+            // 
+            // Detalhes
+            // 
+            this.Detalhes.Location = new System.Drawing.Point(294, 143);
+            this.Detalhes.Name = "Detalhes";
+            this.Detalhes.Size = new System.Drawing.Size(102, 57);
+            this.Detalhes.TabIndex = 2;
+            this.Detalhes.Text = "Detalhes";
+            this.Detalhes.UseVisualStyleBackColor = true;
+            this.Detalhes.Click += new System.EventHandler(this.Detalhes_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(403, 144);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 56);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Detalhes);
+            this.Controls.Add(this.Desligar);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Desligar;
+        private System.Windows.Forms.Button Detalhes;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
